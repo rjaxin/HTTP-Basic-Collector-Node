41a10117962cf30889f43ca544ad6fc4e58cac82
Rob Jackson <robert.jackson@pingidentity.com>
2025-12-19 11:33:39 -0700

    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # HTTP-Basic-Collector-Node
+
+This is an example Custom Node that can be imported into [Ping Advanced Identity Cloud][https://docs.pingidentity.com/pingoneaic/latest/journeys/node-designer.html#import-custom-node] or PingAM to be used in a journey.
+
+The functionality is similar to the out-of-the-box [Zero Login Page Collector Node][https://docs.pingidentity.com/auth-node-ref/latest/zero-page-login-collector.html], however, it will decode a standard Basic Authorization header as per [rfc7617][https://datatracker.ietf.org/doc/html/rfc7617], and push the username and password into the Journey's shared/transient states.
+
+The Custom Node import file can be downloaded [here](HTTP-Basic-Collector-Node.json)
+
+The script source is:
+```javascript
+# src/main.js
+```
---
+++ src/main.js
@@ -0,0 +1,50 @@
+
+/*
+* robert.jackson@pingidentity.com 2025-12-19
+* 
+* THE CODE HEREUNDER IS PROVIDED "AS IS" AND WITHOUT WARRANTY OF ANY KIND. SUCH CODE IS EXPRESSLY EXCLUDED FROM PING IDENTITY'S INDEMNITY 
+* OR SUPPORT OBLIGATIONS, IF ANY, PURSUANT TO THE RELEVANT GOVERNING AGREEMENT. PING IDENTITY AND ITS LICENSORS EXPRESSLY DISCLAIM ALL WARRANTIES, 
+* WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, 
+* AND ANY WARRANTY OF NON-INFRINGEMENT. PING IDENTITY SHALL NOT HAVE ANY LIABILITY ARISING OUT OF OR RELATING TO ANY USE, IMPLEMENTATION OR 
+* CONFIGURATION OF THE SAMPLE CODE.
+*
+* Node binding documentation found at: https://docs.pingidentity.com/pingoneaic/latest/am-scripting/script-bindings.html#common-utils
+*/
+
+var nodeOutcomes = {
+  HAS_CREDENTIALS: "Has Credentials",
+  NO_CREDENTIALS: "No Credentials",
+  ERROR: "Error"
+};
+
+var nodeConfig = {
+  logPrefix: "HTTPBasicAuthCustomNode:"
+}
+
+var authHeader = requestHeaders.get("Authorization").get(0);
+if (authHeader) {
+  var regex = /^Basic\s+([a-zA-Z0-9+\/=]+)$/gmi;
+  var match = regex.exec(authHeader)
+  if (match) {
+    logger.debug("{} Basic Authorization header is present", nodeConfig.logPrefix);
+    try {
+      var decoded = utils.base64.decode(match[1]);
+      var separatorIndex = decoded.indexOf(":");
+      if (separatorIndex !== -1) {
+        var username = decoded.substring(0, separatorIndex)
+        var password = decoded.substring(separatorIndex + 1);
+        nodeState.putShared("username", username);
+        nodeState.putTransient("password", password);
+        action.goTo(nodeOutcomes.HAS_CREDENTIALS);
+      } else {
+        logger.error("{} Decoded string does not contain a colon separator", nodeConfig.logPrefix);
+        action.goTo(nodeOutcomes.ERROR);
+      }
+    } catch (e) {
+      logger.error("{} Error decoding Base64 string: {}", nodeConfig.logPrefix, e.message);
+      action.goTo(nodeOutcomes.ERROR);
+    }
+  }
+} else {
+  action.goTo(nodeOutcomes.NO_CREDENTIALS);
+}
